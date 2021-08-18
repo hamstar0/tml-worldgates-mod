dd5293f478cd217f0fdb026951f517d0592947ac
hamstar0 <webutek@gmail.com>
2021-08-17 20:28:49 -0700

    v1.0.3 - Added API

---
+++ WorldGates/API.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace WorldGates {
+	public class WorldGatesAPI {
+		public static IEnumerable<GateBarrier> GetGateBarriers() {
+			return SoulBarriers.SoulBarriersAPI.GetWorldBarriers()
+				.Where( b => b is GateBarrier )
+				.Select( b => b as GateBarrier );
+		}
+	}
+}
