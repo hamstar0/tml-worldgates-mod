cb680041cd37d0cb6f960f4767f7da985d2fa048
hamstar0 <webutek@gmail.com>
2022-03-30 15:36:47 -0700

    v1.2.6.4 - Updates

--- WorldGates/GateBarrier.cs
+++ WorldGates/GateBarrier.cs
@@ -6,10 +6,11 @@ using Terraria.ModLoader;
 using ModLibsCore.Classes.Errors;
 using ModLibsCore.Libraries.Debug;
 using SoulBarriers;
+using SoulBarriers.Barriers;
 using SoulBarriers.Barriers.BarrierTypes;
 using SoulBarriers.Barriers.BarrierTypes.Rectangular.Access;
 using WorldGates.Packets;
-using SoulBarriers.Barriers;
+
 
 namespace WorldGates {
 	public class GateBarrier : AccessBarrier {
@@ -63,32 +64,6 @@ namespace WorldGates {
 						color: color,
 						isSaveable: isSaveable
 					) {
-			bool preCollide( Barrier barrier, ref double damage ) {
-				return !( barrier is AccessBarrier ) && barrier.IsActive;
-			}
-
-			void postCollide( Barrier otherBarrier, bool isDefaultHit, double damage ) {
-//LogLibraries.Log( "B V B OnBarrierBarrierCollision 2 - " + this.Strength );
-				if( this.Strength > 0d ) {
-					string str;
-					if( (this.Strength % 1d) > 0d ) {
-						str = ((int)this.Strength + (this.Strength % 1d)).ToString("N2");
-					} else {
-						str = ((int)this.Strength).ToString();
-					}
-
-					Main.NewText( "Gate barrier is too strong. +"+str+" strength needed to breach.", Color.Yellow );
-					Main.PlaySound( SoundID.NPCHit53 );
-				} else {
-					//Main.NewText( "Access granted.", Color.Lime );
-					//Main.PlaySound( SoundID.Item94 );
-				}
-			}
-
-			//
-
-			this.OnPreBarrierBarrierCollision.Add( preCollide );
-			this.OnPostBarrierBarrierCollision.Add( postCollide );
 		}
 
 
